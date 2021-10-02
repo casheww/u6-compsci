ed961075096d34322466e7efbbd60dda6560bd4f
casheww <51793722+casheww@users.noreply.github.com>
2021-10-02 20:11:56 +0100

    add array benchmarking

---
+++ ArrayBenchmarking/CustomArray.cs
@@ -0,0 +1,20 @@
+﻿namespace ArrayBenchmarking
+{
+    public readonly struct CustomArray<T>
+    {
+        public CustomArray(uint width, uint height)
+        {
+            _items = new T[width * height];
+            this.width = width;
+        }
+
+        public T this[int x, int y]
+        {
+            get => _items[x + y * width];
+            set => _items[x + y * width] = value;
+        }
+
+        private readonly T[] _items;
+        public readonly uint width;
+    }
+}
