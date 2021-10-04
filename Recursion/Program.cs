d04b18633034e9246c2e1fa58b7a99579a8c517b
casheww <51793722+casheww@users.noreply.github.com>
2021-10-04 12:17:40 +0100

    add recursion stuffs

---
+++ Recursion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run(typeof(FactorialTests));
+        }
+
+    }
+
+    public class FactorialTests
+    {
+        [Benchmark]
+        [ArgumentsSource(nameof(Numbers))]
+        public int FactorialLoop(int n)
+        {
+            for (int i = n - 1; i > 0; i--)
+                n *= i;
+
+            return n;
+        }
+        
+        [Benchmark]
+        [ArgumentsSource(nameof(Numbers))]
+        public int FactorialRecursive(int n)
+        {
+            if (n > 1)
+                n *= FactorialRecursive(n - 1);
+            return n;
+        }
+
+        public IEnumerable<int> Numbers()
+        {
+            yield return 10;
+            yield return 100;
+        }
+    }
+    
+}
