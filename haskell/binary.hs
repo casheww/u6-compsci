c024d18ac38873c93cfe7b728f609abf1edbaced
casheww <51793722+casheww@users.noreply.github.com>
2022-03-23 14:02:56 +0000

    add dec-bin converter

--- haskell/binary.hs
+++ haskell/binary.hs
@@ -1,13 +1,25 @@
 
-decToBin :: Int -> [Int]
-decToBin x = [0]
-
-
-double x = x * 2
+processBit :: Int -> Int -> Int
+processBit dec bit
+    | bit == 0 = dec * 2
+    | otherwise = dec * 2 + 1
 
 binToDec :: [Int] -> Int
-binToDec = foldr (*) 2
+binToDec = foldl processBit 0
+
+
+decToBin :: Int -> [Int]
+decToBin dec
+    | dec == 0 = []
+    | otherwise = do
+        let d = dec `div` 2
+        let r = dec `mod` 2
+        decToBin d ++ [r]
 
 
 main = do
-    print(decToBin 10)
+    let dec = 196
+    print ("dec : " ++ show dec)
+    let bin = decToBin dec
+    print("bin : " ++ show bin)
+    print("dec check : " ++ show (binToDec bin))
