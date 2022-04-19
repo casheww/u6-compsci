fa9be259e24cfd3244c1b98efe70d73e58025bd6
casheww <51793722+casheww@users.noreply.github.com>
2022-04-19 11:23:20 +0100

    do word pair construction task

---
+++ WordFromOtherWord/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace WordFromOtherWord;
+
+static class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter the first word >  ");
+        string first = Console.ReadLine().ToUpper();
+        Console.Write("Enter the second word >  ");
+        string second = Console.ReadLine().ToUpper();
+        
+        foreach (char c in first)
+        {
+            if (second.Contains(c))
+            {
+                int i = second.IndexOf(c);
+                second = second.Substring(0, i) + second.Substring(i + 1, second.Length - i - 1);
+            }
+            else
+            {
+                Console.WriteLine("The first word CANNOT be made of letter from the second word.");
+                return;
+            }
+        }
+
+        Console.WriteLine("The first word CAN be made of letter from the second word.");
+    }
+}
