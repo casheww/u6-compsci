07526b7656bbaa1a72721b704398750e709f4d48
casheww <51793722+casheww@users.noreply.github.com>
2022-06-08 22:59:20 +0100

    do 2020 section b

---
+++ ModalDigit/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace ModalDigit;
+
+public static class Program
+{
+    public static void Main()
+    {
+        int[] frequencies = new int[10];
+        
+        Console.Write("Enter the number of digits to be entered >  ");
+        int digitCount = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < digitCount; i++)
+        {
+            Console.Write($"\nEnter digit number {i+1} >  ");
+            int digit = int.Parse(Console.ReadLine());
+            frequencies[digit]++;
+        }
+
+        int modalFreq = -1;
+        bool multimodal = false;
+        for (int i = 0; i < frequencies.Length; i++)
+        {
+            if (frequencies[i] > modalFreq)
+            {
+                multimodal = false;
+                modalFreq = frequencies[i];
+            }
+            else if (frequencies[i] == modalFreq)
+            {
+                multimodal = true;
+            }
+        }
+
+        if (multimodal)
+            Console.WriteLine("\nData was multimodal");
+        else
+            Console.WriteLine($"\n{modalFreq}");
+    }
+}
+
